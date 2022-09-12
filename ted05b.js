0df93788f67bedebd883bc4d5a24dc810defc088
GABRIEL GOMES <103393275+gabrielgomes1986@users.noreply.github.com>
2022-09-11 23:01:42 -0300

    Add files via upload

---
+++ ted05b.js
@@ -0,0 +1,33 @@
+package example;
+
+public class ContaLogin {
+	
+	private String nome; // Variável de instância
+	private String login;
+	
+	
+	// Método construtor
+//	public ContaLogin(String nome) {
+//		this.nome = nome;
+//	}
+//	
+	
+	// Get e Set de Nome
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//Get e Set de Login
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	
+}
+
